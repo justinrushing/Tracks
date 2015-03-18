98c558fb6de1fff38bb72c4c9bf37793423bb0c2
Justin Rushing <jsrush91@gmail.com>
2015-03-17 22:47:11 -0700

    Created directory structure and created an Xcode project for the Mac and iOS versions of Tracks

---
+++ Client/iOSTracks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSTracks
+//
+//  Created by Justin Rushing on 3/17/15.
+//  Copyright © 2015 Justin Rushing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
